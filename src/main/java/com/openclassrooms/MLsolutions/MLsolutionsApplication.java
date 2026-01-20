a3dfeb84dee9ee86c1ac5eb61706e6fbc47de128
Desiree <desiree.ttt@gmail.com>
2026-01-20 16:56:49 +0100

    init repo

---
+++ src/main/java/com/openclassrooms/MLsolutions/MLsolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.MLsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MLsolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MLsolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/MLsolutions/MLsolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.MLsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MLsolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
